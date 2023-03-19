285deb75de1cfb389f21601e0a27d04581ab824e
Sanahwani <wsana911@gmail.com>
2023-03-19 20:39:49 +0530

    Initial commit

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'brush unit remember news depth tree gospel differ giraffe exact manage unusual',
+  // remember to change this to your own phrase!
+  'https://goerli.infura.io/v3/9d185d6548824015ab8fcace2b70a0a3'
+  // remember to change this to your own endpoint!
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
